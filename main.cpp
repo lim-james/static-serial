5b755c9c994f1c8f8d74a319aa5b723b97b65d8d
James <jameslimbj@gmail.com>
2026-03-20 23:34:06 +0800

    clean up test cases

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
-#include <cstdint>
-#include <cstddef>
-#include <array>
-#include <bit>
-#include <span>
-#include <tuple>
-#include <type_traits>
+#include "static_serial.hpp"
 
-#include <meta>
+#include <cstdint>
+
+template<auto data>
+constexpr bool test_back_and_forth() {
+    using T = decltype(data);
+    auto bytes    = stse::serialize(data);
+    auto restored = stse::deserialize<T>(bytes);
+    return data == restored;
+}
+
+///
+/// 1. Test scalars
+///
+
+static constexpr char   c = '0';
+static constexpr int    i = 0;
+static constexpr float  f = 3.14f;
+static constexpr double d = 123.45;
+static constexpr bool   b = false;
+
+static_assert(test_back_and_forth<c>(), "Back-&-Forth failed for char");
+static_assert(test_back_and_forth<i>(), "Back-&-Forth failed for int");
+static_assert(test_back_and_forth<f>(), "Back-&-Forth failed for float");
+static_assert(test_back_and_forth<d>(), "Back-&-Forth failed for double");
+static_assert(test_back_and_forth<b>(), "Back-&-Forth failed for bool");
+
+///
+/// 2. Test std::array
+///
+
+static constexpr std::array std_arr{1, 2, 3, 4, 5};
+static_assert(test_back_and_forth<std_arr>(), "Back-&-Forth failed for std::array");
+
+///
+/// 3. Test aggregate type
+///
 
 struct Vec3 {
     float x;
@@ -16,6 +45,13 @@ struct Vec3 {
     bool operator==(const Vec3&) const = default;
 };
 
+static constexpr Vec3 position{.x = 0.1, .y = 0.2, .z = 0.3};
+static_assert(test_back_and_forth<position>(), "Back-&-Forth failed for Aggregate Types");
+
+///
+/// 4. Ensure no padding
+///
+
 struct PaddedStruct {
     char a;
     std::uint64_t b;
@@ -23,180 +59,44 @@ struct PaddedStruct {
     bool operator==(const PaddedStruct&) const = default;
 };
 
+static constexpr PaddedStruct padded{'a', 0};
+static constexpr auto bytes = stse::serialize(padded);
+static_assert(bytes.size() != sizeof(PaddedStruct));
+static_assert(bytes.size() == 9);
+static_assert(stse::deserialize<PaddedStruct>(bytes) == padded);
+
 struct Player {
-    uint32_t id;
+    std::uint32_t id;
     Vec3 pos;
     std::array<int, 4> inventory;
 
     bool operator==(const Player&) const = default;
 };
 
-template<typename T>
-concept SerializableScalar = std::is_scalar_v<T>;
+///
+/// 5. Test nested types
+///
 
-template<typename T>
-concept SerializableCArray = std::is_array_v<T> &&
-                             std::is_trivially_copyable_v<std::remove_all_extents_t<T>>;
+static constexpr Player player{
+    .id = 0, 
+    .pos = Vec3{.x = 0.1, .y = 0.2, .z = 0.3},
+    .inventory = {1, 2, 3, 4}
+};
+static_assert(test_back_and_forth<player>(), "Back-&-Forth failed for Nested Types");
+ 
+///
+/// 6. Rejected Types
+///
 
-template<typename T>
-concept SerializableStdArray = requires {
-    typename T::value_type;
-    typename std::tuple_size<T>::type;
-} && std::is_trivially_copyable_v<typename T::value_type>;
+struct NonTrivialPlayer {
+    std::uint32_t  id;
+    std::uint32_t* ptr;
 
-template<typename T>
-concept SerializableArray = SerializableCArray<T> || SerializableStdArray<T>;
+    bool operator==(const NonTrivialPlayer&) const = default;
+};
 
-template<typename T>
-concept SerializableAggregate = std::is_trivially_copyable_v<T> && 
-                                std::is_aggregate_v<T> &&
-                                !SerializableScalar<T> &&
-                                !SerializableArray<T>;
+// static constexpr NonTrivialPlayer non_trivial_player{.id = 0, .ptr = nullptr};
+// static_assert(test_back_and_forth<non_trivial_player>());
 
-template<SerializableScalar T>
-consteval std::size_t size_of() { return std::meta::size_of(^^T); }
-
-template<SerializableCArray T>
-consteval std::size_t size_of() { 
-    return std::extent_v<T> * size_of<std::remove_all_extents_t<T>>();
-}
-
-template<SerializableStdArray T>
-consteval std::size_t size_of() { 
-    return std::tuple_size_v<T> * size_of<typename T::value_type>();
-}
-
-template<SerializableAggregate T>
-consteval std::size_t size_of() { 
-    static constexpr auto data_members = std::define_static_array(
-        std::meta::nonstatic_data_members_of(^^T, std::meta::access_context::current())
-    );
-
-    std::size_t total = 0;
-    template for (constexpr auto member : data_members) {
-        total += size_of<typename[:std::meta::type_of(member):]>();
-    }
-    return total;
-}
-
-template<typename T>
-inline constexpr std::size_t raw_size = size_of<T>();
-
-template<SerializableScalar T>
-constexpr std::span<std::byte> serialize(std::span<std::byte> destination, const T& source) {
-    static constexpr std::size_t value_byte_count = raw_size<T>;
-    using value_buffer_t = std::array<std::byte, value_byte_count>;
-    auto bytes = std::bit_cast<value_buffer_t>(source);
-
-    for (std::size_t i{}; i < value_byte_count; ++i) {
-        destination[i] = bytes[i];
-    }
-
-    return destination.subspan(value_byte_count);
-}
-
-template<SerializableArray T>
-constexpr std::span<std::byte> serialize(std::span<std::byte> destination, const T& source) {
-    for (const auto& item: source) destination = serialize(destination, item);
-    return destination;
-}
-
-template<SerializableAggregate T>
-constexpr std::span<std::byte> serialize(std::span<std::byte> destination, const T& source) {
-    static constexpr auto data_members = std::define_static_array(
-        std::meta::nonstatic_data_members_of(^^T, std::meta::access_context::current())
-    );
-
-    template for (constexpr auto member : data_members) {
-        destination = serialize(destination, source.[:member:]);
-    }
-
-    return destination;
-}
-
-template<typename T> 
-[[nodiscard]] constexpr auto serialize(const T& data) -> std::array<std::byte, raw_size<T>> {
-    auto buffer = std::array<std::byte, raw_size<T>>{};
-    serialize(buffer, data);
-    return buffer;
-}
-
-template<SerializableScalar T>
-constexpr std::span<const std::byte> deserialize(T& destination, std::span<const std::byte> source) {
-    static constexpr std::size_t value_byte_count = raw_size<T>;
-    using value_buffer_t = std::array<std::byte, value_byte_count>;
-    value_buffer_t buffer;
-
-    auto bytes = source.first(value_byte_count);
-    for (std::size_t i{}; i < value_byte_count; ++i) {
-        buffer[i] = bytes[i];
-    }
-
-    destination = std::bit_cast<T>(buffer);
-
-    return source.subspan(value_byte_count);
-}
-
-
-template<SerializableArray T>
-constexpr std::span<const std::byte> deserialize(T& destination, std::span<const std::byte> source) {
-    for (auto& item: destination) source = deserialize(item, source);
-    return source;
-}
-
-template<SerializableAggregate T>
-constexpr std::span<const std::byte> deserialize(T& destination, std::span<const std::byte> source) {
-    static constexpr auto data_members = std::define_static_array(
-        std::meta::nonstatic_data_members_of(^^T, std::meta::access_context::current())
-    );
-
-    template for (constexpr auto member: data_members) {
-        source = deserialize(destination.[:member:], source);
-    }
-
-    return source;
-}
-
-template<typename T>
-[[nodiscard]] constexpr T deserialize(std::span<const std::byte> data) {
-    T parsed;
-    deserialize(parsed, data);
-    return parsed;
-}
-
-template<auto data>
-constexpr bool test_back_and_forth() {
-    using T = decltype(data);
-    auto bytes    = serialize(data);
-    auto restored = deserialize<T>(bytes);
-    return data == restored;
-}
-
-template<typename T>
-bool test_back_and_forth_runtime(const T& data) {
-    auto bytes    = serialize(data);
-    auto restored = deserialize<T>(bytes);
-    return data == restored;
-}
-
-int main() {
-    static constexpr Vec3 position{.x = 0.1, .y = 0.2, .z = 0.3};
-    static_assert(test_back_and_forth<position>(), "Back-&-Forth serialization failed");
-
-    static constexpr std::array arr{1, 2, 3, 4, 5};
-    static_assert(test_back_and_forth<arr>(), "Back-&-Forth serialization failed");
-    
-    static constexpr PaddedStruct padded{'a', 0};
-    constexpr auto bytes = serialize(padded);
-    static_assert(bytes.size() != sizeof(PaddedStruct));
-    static_assert(bytes.size() == 9);
-    static_assert(deserialize<PaddedStruct>(bytes) == padded);
-
-    static constexpr Player player{
-        .id = 0, 
-        .pos = Vec3{.x = 0.1, .y = 0.2, .z = 0.3},
-        .inventory = {1, 2, 3, 4}
-    };
-    static_assert(test_back_and_forth<player>(), "Back-&-Forth serialization failed");
-}
+int main() {}
 
