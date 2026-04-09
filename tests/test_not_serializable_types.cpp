6dcbaf7fabb6eee51f3b9d29edaedc320e87a51b
James <jameslimbj@gmail.com>
2026-04-09 12:42:33 +0800

    create separate test file for unsupported scalars

--- tests/test_not_serializable_types.cpp
+++ tests/test_not_serializable_types.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 
-static_assert(stse::is_serializable_v<int*>             == false);
 static_assert(stse::is_serializable_v<char[16]>         == false);
 static_assert(stse::is_serializable_v<std::string>      == false);
 static_assert(stse::is_serializable_v<std::vector<int>> == false);
---
+++ tests/test_unsupported_scalar.cpp
@@ -0,0 +1,4 @@
+#include "static_serial.hpp"
+
+static_assert(stse::is_serializable_v<void*>          == false);
+static_assert(stse::is_serializable_v<std::nullptr_t> == false);
