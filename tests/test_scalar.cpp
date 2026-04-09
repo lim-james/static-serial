03fb17f5b3bbb4bd496aef3991085795f2ef72b9
James <jameslimbj@gmail.com>
2026-04-09 13:12:38 +0800

    standardise checking serializability before round trip tests

--- tests/test_scalar.cpp
+++ tests/test_scalar.cpp
@@ -1,5 +1,16 @@
 #include "static_serial_test.hpp"
 
+static_assert(stse::is_serializable_v<char>);
+static_assert(stse::is_serializable_v<int>);
+static_assert(stse::is_serializable_v<short>);
+static_assert(stse::is_serializable_v<unsigned int>);
+static_assert(stse::is_serializable_v<float>);
+static_assert(stse::is_serializable_v<double>);
+static_assert(stse::is_serializable_v<long double>);
+static_assert(stse::is_serializable_v<bool>);
+static_assert(stse::is_serializable_v<UnscopedEnum>);
+static_assert(stse::is_serializable_v<ScopedEnum>);
+
 static constexpr char   c = '0';
 static constexpr int    i = 0;
 static constexpr float  f = 3.14f;
@@ -19,14 +30,3 @@ static_assert(stse::test::test_back_and_forth<u>(), "Back-&-Forth failed for uns
 enum class ScopedEnum { option_a, option_b }; 
 static constexpr ScopedEnum e = ScopedEnum::option_a;
 static_assert(stse::test::test_back_and_forth<e>(), "Back-&-Forth failed for scoped enum");
-
-static_assert(stse::is_serializable_v<char>);
-static_assert(stse::is_serializable_v<int>);
-static_assert(stse::is_serializable_v<short>);
-static_assert(stse::is_serializable_v<unsigned int>);
-static_assert(stse::is_serializable_v<float>);
-static_assert(stse::is_serializable_v<double>);
-static_assert(stse::is_serializable_v<long double>);
-static_assert(stse::is_serializable_v<bool>);
-static_assert(stse::is_serializable_v<UnscopedEnum>);
-static_assert(stse::is_serializable_v<ScopedEnum>);
