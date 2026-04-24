99c0b99ab60106a5ec12b35ea25708f515fe1956
James <jameslimbj@gmail.com>
2026-04-24 22:55:29 +0800

    add note that tests have passed

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,8 +2,10 @@
 
 #include <tuple>
 #include <cassert>
+#include <print>
 
 int main() {
     std::tuple sample{1, 2};
     assert(stse::test::test_round_trip(sample));
+    std::println("All tests passed.");
 }
