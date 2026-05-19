507b653c69f03bb420051092c26790bf33acd351
James <jameslimbj@gmail.com>
2026-05-19 20:32:16 +0800

    remove tuple tests from static container tests

--- tests/test_static_container.cpp
+++ tests/test_static_container.cpp
@@ -9,10 +9,6 @@ static_assert(stse::test::test_round_trip<std_arr>(), "Round trip failed for std
 
 static_assert(stse::is_serializable_v<std::tuple<int,int>>);
 
-// _Tuple_impl has a non-structural type
-// static constexpr std::tuple std_tuple{1, 2, 3, 4, 5};
-// static_assert(stse::test::test_round_trip<std_tuple>(), "Round trip failed for std::tuple");
-
 static_assert(stse::is_serializable_v<std::stack<int, std::array<int, 10>>>);
 static_assert(stse::is_serializable_v<std::queue<int, std::array<int, 10>>>);
 static_assert(stse::is_serializable_v<std::priority_queue<int, std::array<int, 10>>>);
