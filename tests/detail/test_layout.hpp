ec36378a71d640e7bf96748583b58937cac22583
james <jameslimbj@gmail.com>
2026-06-21 23:09:20 +0800

    standardise structs for testing & validate contract layout
    Signed-off-by: james <jameslimbj@gmail.com>

---
+++ tests/detail/test_layout.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include "tests/types.hpp"
+#include "stse/detail/layout.hpp"
+
+#include <cstdint>
+#include <array>
+
+namespace stse::tests {
+
+static_assert(stse::detail::raw_size<char>   == sizeof(char));
+static_assert(stse::detail::raw_size<bool>   == sizeof(bool));
+static_assert(stse::detail::raw_size<int>    == sizeof(int));
+static_assert(stse::detail::raw_size<double> == sizeof(double));
+static_assert(stse::detail::raw_size<float>  == sizeof(float));
+
+static_assert(stse::detail::raw_size<std::uint8_t>  == sizeof(std::uint8_t));
+static_assert(stse::detail::raw_size<std::uint16_t> == sizeof(std::uint16_t));
+static_assert(stse::detail::raw_size<std::uint32_t> == sizeof(std::uint32_t));
+static_assert(stse::detail::raw_size<std::uint64_t> == sizeof(std::uint64_t));
+
+
+static_assert(stse::detail::raw_size<FlatCompactAggregate_3b>  == sizeof(FlatCompactAggregate_3b));
+static_assert(stse::detail::raw_size<FlatCompactAggregate_3b>  == 3);
+
+static_assert(stse::detail::raw_size<FlatCompactAggregate_12b> == sizeof(FlatCompactAggregate_12b));
+static_assert(stse::detail::raw_size<FlatCompactAggregate_12b> == 12);
+
+
+static_assert(stse::detail::raw_size<FlatInternalPaddedAggregate_8b> != sizeof(FlatInternalPaddedAggregate_8b));
+static_assert(stse::detail::raw_size<FlatInternalPaddedAggregate_8b> == 6);
+
+
+static_assert(stse::detail::raw_size<FlatTailPaddedAggregate_12b> != sizeof(FlatTailPaddedAggregate_12b));
+static_assert(stse::detail::raw_size<FlatTailPaddedAggregate_12b> == 6);
+
+
+static_assert(stse::detail::raw_size<WithCompactArray_4b> == sizeof(WithCompactArray_4b));
+static_assert(stse::detail::raw_size<WithCompactArray_4b> == 4);
+
+static_assert(stse::detail::raw_size<WithPaddedArray_12b> != sizeof(WithPaddedArray_12b));
+static_assert(stse::detail::raw_size<WithPaddedArray_12b> == 9);
+
+
+static_assert(stse::detail::raw_size<NestedCompactAggregate_5b> == sizeof(NestedCompactAggregate_5b));
+static_assert(stse::detail::raw_size<NestedCompactAggregate_5b> == 5);
+
+static_assert(stse::detail::raw_size<NestedPaddedAggregate_12b> != sizeof(NestedPaddedAggregate_12b));
+static_assert(stse::detail::raw_size<NestedPaddedAggregate_12b> == 7);
+
+static_assert(stse::detail::raw_size<DeeplyNestedAggregate_16b> != sizeof(DeeplyNestedAggregate_16b));
+static_assert(stse::detail::raw_size<DeeplyNestedAggregate_16b> == 13);
+
+static_assert(stse::detail::raw_size<ArrayOfStructsAggregate_8b> == sizeof(ArrayOfStructsAggregate_8b));
+static_assert(stse::detail::raw_size<ArrayOfStructsAggregate_8b> == 8);
+
+static_assert(stse::detail::raw_size<std::array<_1b, N>> == N);
+static_assert(stse::detail::raw_size<std::array<_4b, N>> == 4 * N);
+static_assert(stse::detail::raw_size<std::array<_8b, N>> == 8 * N);
+static_assert(stse::detail::raw_size<std::array<FlatCompactAggregate_3b, N>>
+    == stse::detail::raw_size<FlatCompactAggregate_3b> * N);
+
+static_assert(stse::detail::count_byte_ranges<FlatCompactAggregate_3b>() == 1);
+static_assert(stse::detail::byte_layout_of<FlatCompactAggregate_3b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 3});
+
+static_assert(stse::detail::count_byte_ranges<FlatCompactAggregate_12b>() == 1);
+static_assert(stse::detail::byte_layout_of<FlatCompactAggregate_12b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 12});
+
+static_assert(stse::detail::count_byte_ranges<FlatInternalPaddedAggregate_8b>() == 2);
+static_assert(stse::detail::byte_layout_of<FlatInternalPaddedAggregate_8b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 2});
+static_assert(stse::detail::byte_layout_of<FlatInternalPaddedAggregate_8b>[1] ==
+    stse::detail::ByteRange{.struct_offset = 4, .wire_offset = 2, .count = 4});
+
+static_assert(stse::detail::count_byte_ranges<FlatTailPaddedAggregate_12b>() == 2);
+static_assert(stse::detail::byte_layout_of<FlatTailPaddedAggregate_12b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 1});
+static_assert(stse::detail::byte_layout_of<FlatTailPaddedAggregate_12b>[1] ==
+    stse::detail::ByteRange{.struct_offset = 4, .wire_offset = 1, .count = 5});
+
+static_assert(stse::detail::count_byte_ranges<WithCompactArray_4b>() == 1);
+static_assert(stse::detail::byte_layout_of<WithCompactArray_4b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 4});
+
+static_assert(stse::detail::count_byte_ranges<WithPaddedArray_12b>() == 2);
+static_assert(stse::detail::byte_layout_of<WithPaddedArray_12b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 1});
+static_assert(stse::detail::byte_layout_of<WithPaddedArray_12b>[1] ==
+    stse::detail::ByteRange{.struct_offset = 4, .wire_offset = 1, .count = 8});
+
+static_assert(stse::detail::count_byte_ranges<NestedCompactAggregate_5b>() == 1);
+static_assert(stse::detail::byte_layout_of<NestedCompactAggregate_5b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 5});
+
+static_assert(stse::detail::count_byte_ranges<NestedPaddedAggregate_12b>() == 3);
+static_assert(stse::detail::byte_layout_of<NestedPaddedAggregate_12b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 1});
+static_assert(stse::detail::byte_layout_of<NestedPaddedAggregate_12b>[1] ==
+    stse::detail::ByteRange{.struct_offset = 4, .wire_offset = 1, .count = 2});
+static_assert(stse::detail::byte_layout_of<NestedPaddedAggregate_12b>[2] ==
+    stse::detail::ByteRange{.struct_offset = 8, .wire_offset = 3, .count = 4});
+
+static_assert(stse::detail::count_byte_ranges<DeeplyNestedAggregate_16b>() == 2);
+static_assert(stse::detail::byte_layout_of<DeeplyNestedAggregate_16b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 5});
+static_assert(stse::detail::byte_layout_of<DeeplyNestedAggregate_16b>[1] ==
+    stse::detail::ByteRange{.struct_offset = 8, .wire_offset = 5, .count = 8});
+
+static_assert(stse::detail::count_byte_ranges<ArrayOfStructsAggregate_8b>() == 1);
+static_assert(stse::detail::byte_layout_of<ArrayOfStructsAggregate_8b>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 8});
+
+static_assert(stse::detail::count_byte_ranges<std::array<_1b, N>>() == 1);
+static_assert(stse::detail::byte_layout_of<std::array<_1b, N>>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = N});
+
+static_assert(stse::detail::count_byte_ranges<std::array<_4b, N>>() == 1);
+static_assert(stse::detail::byte_layout_of<std::array<_4b, N>>[0] ==
+    stse::detail::ByteRange{.struct_offset = 0, .wire_offset = 0, .count = 4 * N});
+
+// [a ][b ][pad   ][c             ][a ][b ][pad   ][c             ][a ][b ][pad   ][c             ]
+// [00][01][02][03][04][05][06][07][08][09][10][11][12][13][14][15][16][17][18][19][20][21][22][23]
+// [======][pad   ][======================][pad   ][======================][pad   ][==============]
+
+static_assert(stse::detail::count_byte_ranges<std::array<FlatInternalPaddedAggregate_8b, 3>>() == 4);
+static_assert(stse::detail::byte_layout_of<std::array<FlatInternalPaddedAggregate_8b, 3>>[0] ==
+    stse::detail::ByteRange{.struct_offset =  0, .wire_offset =  0, .count = 2});
+static_assert(stse::detail::byte_layout_of<std::array<FlatInternalPaddedAggregate_8b, 3>>[1] ==
+    stse::detail::ByteRange{.struct_offset =  4, .wire_offset =  2, .count = 6});
+static_assert(stse::detail::byte_layout_of<std::array<FlatInternalPaddedAggregate_8b, 3>>[2] ==
+    stse::detail::ByteRange{.struct_offset = 12, .wire_offset =  8, .count = 6});
+static_assert(stse::detail::byte_layout_of<std::array<FlatInternalPaddedAggregate_8b, 3>>[3] ==
+    stse::detail::ByteRange{.struct_offset = 20, .wire_offset = 14, .count = 4});
+
+} // namespace stse::tests
--- tests/types.hpp
+++ tests/types.hpp
@@ -1,53 +1,207 @@
 #pragma once
 
 #include <type_traits>
+#include <array>
+#include <cstdint>
+#include <cstddef>
 
 namespace stse::tests {
 
 static constexpr std::size_t N = 12;
 
-using _1b = std::uint8_t;  
+using _1b = std::uint8_t;
 static_assert(sizeof(_1b)  == 1);
 static_assert(alignof(_1b) == 1);
 
-using _2b = std::uint16_t; 
+using _2b = std::uint16_t;
 static_assert(sizeof(_2b)  == 2);
 static_assert(alignof(_2b) == 2);
 
-using _4b = std::uint32_t; 
+using _4b = std::uint32_t;
 static_assert(sizeof(_4b)  == 4);
 static_assert(alignof(_4b) == 4);
 
-using _8b = std::uint64_t; 
+using _8b = std::uint64_t;
 static_assert(sizeof(_8b)  == 8);
 static_assert(alignof(_8b) == 8);
 
-enum UnscopedEnum { option_a, option_b }; 
-enum class ScopedEnum { option_a, option_b }; 
+enum UnscopedEnum { option_a, option_b };
+enum class ScopedEnum { option_a, option_b };
 
 static_assert(false == std::is_scoped_enum_v<UnscopedEnum>);
 static_assert(std::is_scoped_enum_v<ScopedEnum>);
 
-struct NotTriviallyCopyable {NotTriviallyCopyable(const NotTriviallyCopyable&){}};
+struct NotTriviallyCopyable { NotTriviallyCopyable(const NotTriviallyCopyable&) {} };
 static_assert(false == std::is_trivially_copyable_v<NotTriviallyCopyable>);
 
 struct PolymorphicParent { virtual void boo(); };
-struct PolymorphicChild: PolymorphicParent { void boo() override; };
+struct PolymorphicChild : PolymorphicParent { void boo() override; };
 static_assert(std::is_polymorphic_v<PolymorphicParent>);
 static_assert(std::is_polymorphic_v<PolymorphicChild>);
 
-struct FlatAggregate { 
-    _1b a; _4b b; _1b c; 
-    bool operator==(const FlatAggregate&) const = default;
+/// Byte layout : [a ][b ][c ]
+///               [00][01][02]
+/// Byte size   : 3
+/// Struct size : 3
+struct FlatCompactAggregate_3b {
+    _1b a, b, c;
+    bool operator==(const FlatCompactAggregate_3b&) const = default;
 };
-static_assert(std::is_aggregate_v<FlatAggregate>);
-static_assert(std::is_trivially_copyable_v<FlatAggregate>);
 
-struct B { _1b a; _1b b; _4b c; };
-struct C { _1b a; _1b b; std::array<_1b, N> c; };
-struct D { _1b a; std::array<_4b, N> b; _4b c; };
-struct E { B a; std::array<_8b, N> b; C c; };
-struct F { _1b a; _4b b; _4b c; _1b d; _2b e; };
-struct G { _1b a; _2b b; _8b c; }; 
+static_assert(std::is_aggregate_v<FlatCompactAggregate_3b>);
+static_assert(std::is_trivially_copyable_v<FlatCompactAggregate_3b>);
+static_assert(sizeof(FlatCompactAggregate_3b)   == 3);
+static_assert(alignof(FlatCompactAggregate_3b)  == 1);
+static_assert(offsetof(FlatCompactAggregate_3b, a) == 0);
+static_assert(offsetof(FlatCompactAggregate_3b, b) == 1);
+static_assert(offsetof(FlatCompactAggregate_3b, c) == 2);
 
-}
+/// Byte layout : [a             ][b             ][c             ]
+///               [00][01][02][03][04][05][06][07][08][09][10][11]
+/// Byte size   : 12
+/// Struct size : 12
+struct FlatCompactAggregate_12b {
+    _4b a, b, c;
+    bool operator==(const FlatCompactAggregate_12b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<FlatCompactAggregate_12b>);
+static_assert(std::is_trivially_copyable_v<FlatCompactAggregate_12b>);
+static_assert(sizeof(FlatCompactAggregate_12b)   == 12);
+static_assert(alignof(FlatCompactAggregate_12b)  == 4);
+static_assert(offsetof(FlatCompactAggregate_12b, a) == 0);
+static_assert(offsetof(FlatCompactAggregate_12b, b) == 4);
+static_assert(offsetof(FlatCompactAggregate_12b, c) == 8);
+
+/// Byte layout : [a ][b ][pad   ][c             ]
+///               [00][01][02][03][04][05][06][07]
+/// Byte size   : 6
+/// Struct size : 8
+struct FlatInternalPaddedAggregate_8b {
+    _1b a; _1b b; _4b c;
+    bool operator==(const FlatInternalPaddedAggregate_8b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<FlatInternalPaddedAggregate_8b>);
+static_assert(std::is_trivially_copyable_v<FlatInternalPaddedAggregate_8b>);
+static_assert(sizeof(FlatInternalPaddedAggregate_8b)   == 8);
+static_assert(alignof(FlatInternalPaddedAggregate_8b)  == 4);
+static_assert(offsetof(FlatInternalPaddedAggregate_8b, a) == 0);
+static_assert(offsetof(FlatInternalPaddedAggregate_8b, b) == 1);
+static_assert(offsetof(FlatInternalPaddedAggregate_8b, c) == 4); // +2b pad before c
+
+/// Byte layout : [a ][pad       ][b             ][c ][pad       ]
+///               [00][01][02][03][04][05][06][07][08][09][10][11]
+/// Byte size   : 6
+/// Struct size : 12
+struct FlatTailPaddedAggregate_12b {
+    _1b a; _4b b; _1b c;
+    bool operator==(const FlatTailPaddedAggregate_12b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<FlatTailPaddedAggregate_12b>);
+static_assert(std::is_trivially_copyable_v<FlatTailPaddedAggregate_12b>);
+static_assert(sizeof(FlatTailPaddedAggregate_12b)   == 12);
+static_assert(alignof(FlatTailPaddedAggregate_12b)  == 4);
+static_assert(offsetof(FlatTailPaddedAggregate_12b, a) == 0);
+static_assert(offsetof(FlatTailPaddedAggregate_12b, b) == 4); // +3b pad before b
+static_assert(offsetof(FlatTailPaddedAggregate_12b, c) == 8); // +3b tail pad after c
+
+/// Byte layout : [a ][b0][b1][b2]
+///               [00][01][02][03]
+/// Byte size   : 4
+/// Struct size : 4
+struct WithCompactArray_4b {
+    _1b a; std::array<_1b, 3> b;
+    bool operator==(const WithCompactArray_4b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<WithCompactArray_4b>);
+static_assert(std::is_trivially_copyable_v<WithCompactArray_4b>);
+static_assert(sizeof(WithCompactArray_4b)   == 4);
+static_assert(alignof(WithCompactArray_4b)  == 1);
+static_assert(offsetof(WithCompactArray_4b, a) == 0);
+static_assert(offsetof(WithCompactArray_4b, b) == 1);
+
+/// Byte layout : [a ][pad       ][b0            ][b1            ]
+///               [00][01][02][03][04][05][06][07][08][09][10][11]
+/// Byte size   : 9
+/// Struct size : 12
+struct WithPaddedArray_12b {
+    _1b a; std::array<_4b, 2> b;
+    bool operator==(const WithPaddedArray_12b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<WithPaddedArray_12b>);
+static_assert(std::is_trivially_copyable_v<WithPaddedArray_12b>);
+static_assert(sizeof(WithPaddedArray_12b)   == 12);
+static_assert(alignof(WithPaddedArray_12b)  == 4);
+static_assert(offsetof(WithPaddedArray_12b, a) == 0);
+static_assert(offsetof(WithPaddedArray_12b, b) == 4); // +3b pad before b
+
+/// Byte layout : [a         ][b ][c ]
+///               [00][01][02][03][04]
+/// Byte size   : 5
+/// Struct size : 5
+struct NestedCompactAggregate_5b {
+    FlatCompactAggregate_3b a; _1b b, c;
+    bool operator==(const NestedCompactAggregate_5b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<NestedCompactAggregate_5b>);
+static_assert(std::is_trivially_copyable_v<NestedCompactAggregate_5b>);
+static_assert(sizeof(NestedCompactAggregate_5b)   == 5);
+static_assert(alignof(NestedCompactAggregate_5b)  == 1);
+static_assert(offsetof(NestedCompactAggregate_5b, a) == 0);
+static_assert(offsetof(NestedCompactAggregate_5b, b) == 3);
+static_assert(offsetof(NestedCompactAggregate_5b, c) == 4);
+
+/// Byte layout : [a ][pad       ][ba][bb][pad   ][bc            ]
+///               [00][01][02][03][04][05][06][07][08][09][10][11]
+/// Byte size   : 7
+/// Struct size : 12
+struct NestedPaddedAggregate_12b {
+    _1b a; FlatInternalPaddedAggregate_8b b;
+    bool operator==(const NestedPaddedAggregate_12b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<NestedPaddedAggregate_12b>);
+static_assert(std::is_trivially_copyable_v<NestedPaddedAggregate_12b>);
+static_assert(sizeof(NestedPaddedAggregate_12b)   == 12);
+static_assert(alignof(NestedPaddedAggregate_12b)  == 4);
+static_assert(offsetof(NestedPaddedAggregate_12b, a) == 0);
+static_assert(offsetof(NestedPaddedAggregate_12b, b) == 4); // +3b pad before b
+
+/// Byte layout : [aa        ][ab][ac][pad       ][b                             ]
+///               [00][01][02][03][04][05][06][07][08][09][10][11][12][13][14][15]
+/// Byte size   : 13
+/// Struct size : 16
+struct DeeplyNestedAggregate_16b {
+    NestedCompactAggregate_5b a; _8b b;
+    bool operator==(const DeeplyNestedAggregate_16b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<DeeplyNestedAggregate_16b>);
+static_assert(std::is_trivially_copyable_v<DeeplyNestedAggregate_16b>);
+static_assert(sizeof(DeeplyNestedAggregate_16b)   == 16);
+static_assert(alignof(DeeplyNestedAggregate_16b)  == 8);
+static_assert(offsetof(DeeplyNestedAggregate_16b, a) == 0);
+static_assert(offsetof(DeeplyNestedAggregate_16b, b) == 8); // +3b pad before b
+
+/// Byte layout : [aa][ab][ac][aa][ab][ac][b     ]
+///               [00][01][02][03][04][05][06][07]
+/// Byte size   : 8
+/// Struct size : 8
+struct ArrayOfStructsAggregate_8b {
+    std::array<FlatCompactAggregate_3b, 2> a; _2b b;
+    bool operator==(const ArrayOfStructsAggregate_8b&) const = default;
+};
+
+static_assert(std::is_aggregate_v<ArrayOfStructsAggregate_8b>);
+static_assert(std::is_trivially_copyable_v<ArrayOfStructsAggregate_8b>);
+static_assert(sizeof(ArrayOfStructsAggregate_8b)   == 8);
+static_assert(alignof(ArrayOfStructsAggregate_8b)  == 2);
+static_assert(offsetof(ArrayOfStructsAggregate_8b, a) == 0);
+static_assert(offsetof(ArrayOfStructsAggregate_8b, b) == 6);
+
+} // namespace stse::tests
