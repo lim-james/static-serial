0a63afd2027f50e726a90349ceb3accb92610707
james <jameslimbj@gmail.com>
2026-06-22 13:01:52 +0800

    note on loop usage
    Signed-off-by: james <jameslimbj@gmail.com>

--- include/stse/detail/memory.hpp
+++ include/stse/detail/memory.hpp
@@ -7,7 +7,10 @@ namespace stse::detail {
 
 constexpr void constexpr_memcpy(std::byte* destination, const std::byte* source, std::size_t count) {
     if consteval {
-        for (std::size_t i = 0; i < count; ++i) {
+        // note: intentionally kept as loop rather than std::copy_n 
+        // when tested on Compiler Explorer on -O3, loops yield 
+        // better optimisation options such as vectorisation
+        for (std::size_t i{}; i < count; ++i) {
             destination[i] = source[i];
         }
     } else {
